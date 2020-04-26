baa4b0d91232afbc580e363ef5fcefa429fba4de
unknown <taofeeq.issa076@gmail.com>
2020-04-26 15:04:02 +0100

    End of Module 6

--- DatingApp-Spa/src/app/_services/auth.service.ts
+++ DatingApp-Spa/src/app/_services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {JwtHelperService} from '@auth0/angular-jwt';
 import {map} from 'rxjs/operators';
 
 @Injectable({
@@ -7,6 +8,8 @@ import {map} from 'rxjs/operators';
 })
 export class AuthService {
   baseUrl = 'http://localhost:5000/api/auth/';
+  jwtHelper = new JwtHelperService();
+  decodedToken: any;
 
 constructor(private http: HttpClient) {}
 
@@ -17,11 +20,17 @@ login(model: any) {
         const user = response;
         if (user){
           localStorage.setItem('token', user.token);
+          this.decodedToken = this.jwtHelper.decodeToken(user.token);
+          console.log (this.decodedToken);
         }
       })
     );
   }
   register(model: any){
     return this.http.post(this.baseUrl + 'register', model);
+  }
+  loggedIn(){
+    const token = localStorage.getItem('token');
+    return !this.jwtHelper.isTokenExpired(token);
   }
 }
--- DatingApp-Spa/src/app/app.module.ts
+++ DatingApp-Spa/src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
-import { ErrorInterceptor, ErrorInterceptorProvider } from './_services/error.interceptor';
+import { ErrorInterceptorProvider } from './_services/error.interceptor';
+import { AlertifyService } from './_services/alertify.service';
 
 @NgModule({
    declarations: [
@@ -19,12 +22,15 @@ import { ErrorInterceptor, ErrorInterceptorProvider } from './_services/error.in
    ],
    imports: [
       BrowserModule,
+      BrowserAnimationsModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+      BsDropdownModule.forRoot()
    ],
    providers: [
       AuthService,
-      ErrorInterceptorProvider
+      ErrorInterceptorProvider,
+      AlertifyService
    ],
    bootstrap: [
       AppComponent
